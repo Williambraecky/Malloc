bd38605fd426e2d6760adb82ed73219314dfa19c
William Braeckman <wbraeckm@student.s19.be>
2020-02-12 15:20:51 +0100

    Update test

--- test/main.c
+++ test/main.c
@@ -6,7 +6,7 @@
 /*   By: wbraeckm <wbraeckm@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/11 16:24:03 by wbraeckm          #+#    #+#             */
-/*   Updated: 2020/02/11 23:18:25 by wbraeckm         ###   ########.fr       */
+/*   Updated: 2020/02/12 15:19:15 by wbraeckm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,23 @@
 
 int		main(void)
 {
+	t_blk	*blk;
 	void	*ptr;
 	void	*ptr2;
 	size_t	i;
 
 	ptr = malloc(16);
+	blk = g_blks;
 	ptr2 = malloc(96);
+	i = 1000;
+	while (i--)
+	{
+		ptr = malloc(3);
+		free(ptr);
+	}
+	free(ptr2);
 	show_alloc_mem();
-	ptr2 = realloc(ptr2, 63);
+	ptr2 = malloc(64);
 	ptr = malloc(16);
 	ptr = malloc(16);
 	ptr = malloc(16);
